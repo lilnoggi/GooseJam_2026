091f41165ab8d2328887f2a85122f771166b0e85
Mani <itsamanihd@gmail.com>
2026-08-12 11:27:02 +0100

    EnemyAI Brain
    Created a simple script for calculating probability of calling cheat. Has placeholders. Will need to be updated with actual logic later.

---
+++ Assets/_Scripts/EnemyAI.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private EnemyProfile _activeProfile;
+
+    // This method will be called by TurnManager.cs during Phase 2
+    public bool DecideToChallenge(CardSuit claimedSuit, int claimedValue)
+    {
+        // If a standard minion, execute standard logic
+        if (!_activeProfile.IsBoss)
+        {
+            return RunStandardProbability(claimedSuit, claimedValue);
+        }
+
+        // If a boss, execute their unique logic
+        switch (_activeProfile.TypeOfBoss)
+        {
+            case BossType.FreddyFox:
+                return FreddyFoxLogic(claimedSuit, claimedValue);
+
+            case BossType.BanditWolf:
+                return BanditWolfLogic(claimedSuit, claimedValue);
+
+            case BossType.LucySwan:
+                return LucySwanLogic(claimedSuit, claimedValue);
+
+            default:
+                return RunStandardProbability(claimedSuit, claimedValue);
+        }
+    }   
+
+    private bool RunStandardProbability(CardSuit suit, int value)
+    {
+        // Standard logic goes here, multipled by _activeProfile.SkepticismMultipler
+        return false; // PLACEHOLDER
+
+        // TODO: Probability logic
+    }
+
+    private bool FreddyFoxLogic(CardSuit suit, int value)
+    {
+        // Heavily weights towards calling bluff on high-damage Blood cards
+        if (suit == CardSuit.Blood && value > 10)
+        {
+            return true; // Almost always challenges this
+        }
+        
+        return RunStandardProbability(suit, value);
+    }
+
+    private bool BanditWolfLogic(CardSuit suit, int value)
+    {
+        // Almost always accepts Bone claims to keep attacking
+        if (suit == CardSuit.Bone)
+        {
+            return false; // Accept without checking
+        }
+
+        return RunStandardProbability(suit, value);
+    }
+
+    private bool LucySwanLogic(CardSuit suit, int value)
+    {
+        // Dynamic AI. Remembers past bluffs and adapts her probability per suit
+        // TODO: Implement this & add tracking variables later
+        return RunStandardProbability(suit, value);
+    }
+}
