b2d1dd3fd1ab404a373cd259846ec1202805d29f
Mani <itsamanihd@gmail.com>
2026-08-14 22:29:38 +0100

    Card Textures + Updated CardVisuals
    Card visuals now changes the image to the suit

--- Assets/_Scripts/Logic/CardVisuals.cs
+++ Assets/_Scripts/Logic/CardVisuals.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class CardVisuals : MonoBehaviour
 {
-    [SerializeField] private TMP_Text _rankText;
-    [SerializeField] private TMP_Text _suitText;
+    [Header("Text References")]
+    [SerializeField] private TMP_Text _rankTextUpper;
+    [SerializeField] private TMP_Text _rankTextLower;
+
+    [Header("Image References")]
+    [SerializeField] private Image _suitImage;
+
+    [Header("Suit Sprites")]
+    [SerializeField] private Sprite _bloodSprite;
+    [SerializeField] private Sprite _boneSprite;
+    [SerializeField] private Sprite _featherSprite;
+    [SerializeField] private Sprite _rotSprite; 
 
     /// <summary>
     /// Reads the CardData and updates the 3D text meshes on the prefab
@@ -16,14 +28,45 @@ public class CardVisuals : MonoBehaviour
             return;
         }
 
-        if (_rankText != null)
+        // Update both the top-left and bottom-right rank numbers
+        if (_rankTextUpper != null)
         {
-            _rankText.text = cardData.RankDisplayName;
+            _rankTextUpper.text = cardData.RankDisplayName;
         }
 
-        if (_suitText != null)
+        if (_rankTextLower != null)
         {
-            _suitText.text = cardData.Suit.ToString().ToUpper();
+            _rankTextLower.text = cardData.RankDisplayName;
+        }
+
+        // Dynamically assign the correct sprite
+        if (_suitImage != null)
+        {
+            _suitImage.sprite = GetSuitSprite(cardData.Suit);
+        }
+    }
+
+    /// <summary>
+    /// Returns the correct sprite based on the suit eneum
+    /// </summary>
+    private Sprite GetSuitSprite(CardSuit suit)
+    {
+        switch (suit)
+        {
+            case CardSuit.Blood: 
+                return _bloodSprite;
+            
+            case CardSuit.Bone:
+                return _boneSprite;
+
+            case CardSuit.Rot:
+                return _rotSprite;
+
+            case CardSuit.Feather:
+                return _featherSprite;
+
+            default:
+                return null; 
         }
     }
 }
