749b3c56b6d8906950d7e09c8609bc5481651581
Mani <itsamanihd@gmail.com>
2026-08-23 17:11:00 +0100

    Intro Story Scene Dialogue SFX

--- Assets/_Scripts/Managers/StoryIntroManager.cs
+++ Assets/_Scripts/Managers/StoryIntroManager.cs
@@ -121,6 +121,11 @@ public class StoryIntroManager : MonoBehaviour
         foreach (char letter in line.ToCharArray())
         {
             _dialogueText.text += letter;
+
+            if (letter != ' ')
+            {
+                AudioManager.Instance.PlaySFX(SFXType.DialogueBlip);
+            }
             yield return new WaitForSeconds(_typingSpeed);
         }
 
@@ -134,6 +139,8 @@ public class StoryIntroManager : MonoBehaviour
     /// </summary>
     public void OnActionButtonClicked()
     {
+        AudioManager.Instance.PlaySFX(SFXType.Select);
+
         _currentSlideIndex++;
 
         // If there are more lines, play the next one
